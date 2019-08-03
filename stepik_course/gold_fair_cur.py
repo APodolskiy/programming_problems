d2b692be3814d19151dc06a88484b3472f76e363
APodolskiy <originalea@yandex.ru>
2019-08-03 21:47:06 +0300

    Fix search for the case of big sum of gold bars.

--- stepik_course/gold_fair_cur.py
+++ stepik_course/gold_fair_cur.py
@@ -2,9 +2,16 @@ from typing import List
 
 
 def fair_cut(arr: List[int]) -> int:
+    if len(arr) <= 30:
+        return fair_cut_brute(arr)
+    return fair_cut_dynamic(arr)
+
+
+def fair_cut_dynamic(arr: List[int]) -> int:
     n = len(arr)
     assert n > 0
     inf = sum(arr) + 1
+
     dp = [[0 for _ in range(inf // 2)] for _ in range(n + 1)]
 
     dp[0][0] = 0
@@ -19,6 +26,22 @@ def fair_cut(arr: List[int]) -> int:
     return sum(arr) - 2*best_w
 
 
+def fair_cut_brute(arr: List[int]) -> int:
+    n = len(arr)
+    assert n <= 30
+    best_cut = sum(arr) + 1
+    for mask in range(2**n - 1):
+        cur_sum = 0
+        for i in range(n):
+            if mask & (1 << i):
+                cur_sum += arr[i]
+            else:
+                cur_sum -= arr[i]
+        if abs(cur_sum) < best_cut:
+            best_cut = abs(cur_sum)
+    return best_cut
+
+
 if __name__ == '__main__':
     arr = [1, 2, 3, 4, 5]
     print(fair_cut(arr))
@@ -29,6 +52,11 @@ if __name__ == '__main__':
         print(fair_cut(arr))
 
     with open('problems/gold2.in', 'r') as fp:
+        _ = fp.readline()
+        arr = [int(s) for s in fp.readline().strip().split()]
+        print(fair_cut(arr))
+
+    with open('problems/gold3.in', 'r') as fp:
         _ = fp.readline()
         arr = [int(s) for s in fp.readline().strip().split()]
         print(fair_cut(arr))
