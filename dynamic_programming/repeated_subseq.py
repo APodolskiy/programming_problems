a691f4ec0cea8a557ccad4017856d6e7243be78f
APodolskiy <originalea@yandex.ru>
2020-04-03 20:03:25 +0300

    Find longest repeated subsequence.

--- dynamic_programming/repeated_subseq.py
+++ dynamic_programming/repeated_subseq.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from typing import Optional
 
 
 def exists_sol(s: str) -> bool:
@@ -24,6 +25,48 @@ def exists_sol(s: str) -> bool:
     return res
 
 
+def find_longest_repeated_subseq(s: str) -> str:
+    """
+    Finds repeated subsequence in a given sequence.
+    If there are no such subsequence then it returns empty string.
+    :param s: string to be analyzed
+    :return: repeated subsequence or empty string if there is none such substring.
+    """
+    # Finding longest common sub-sequence
+    seq1 = list(s)
+    seq2 = list(s)
+    size = len(seq1)
+    if size <= 1:
+        return None
+
+    # Dynamic programming
+    dp = [[0 for _ in range(size + 1)] for _ in range(size + 1)]
+    max_val = 0
+    i_idx, j_idx = 0, 0
+    for i in range(1, size + 1):
+        for j in range(1, size + 1):
+            if i == j:
+                continue
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            if seq1[i - 1] == seq2[j - 1]:
+                dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i][j])
+    i_idx, j_idx = size - 1, size
+    max_val = dp[i_idx][j_idx]
+    res_seq = []
+    while max_val > 0:
+        if seq1[i_idx - 1] == seq2[j_idx - 1]:
+            res_seq.append(seq1[i_idx - 1])
+            max_val -= 1
+            i_idx, j_idx = i_idx - 1, j_idx - 1
+        else:
+            if dp[i_idx - 1][j_idx] > dp[i_idx][j_idx - 1]:
+                i_idx = i_idx - 1
+            else:
+                j_idx = j_idx - 1
+    res_seq.reverse()
+    return "".join(res_seq)
+
+
 if __name__ == "__main__":
     assert exists_sol('ABCABD')
     assert exists_sol('ABBB')
@@ -32,3 +75,9 @@ if __name__ == "__main__":
     assert exists_sol('ABCDACB')
     assert not exists_sol('ABCD')
     assert exists_sol('AAA')
+
+    assert len(find_longest_repeated_subseq('ABCABD')) == 2
+    assert len(find_longest_repeated_subseq('AAA')) == 2
+    print(find_longest_repeated_subseq('ABCABD'))
+    print(find_longest_repeated_subseq('AAA'))
+    print(find_longest_repeated_subseq('ABCDACB'))
